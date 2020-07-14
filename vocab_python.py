5d1a426463b631d71d1e2fe885341cdf733b1947
Rod <bhazaev@gmail.com>
2020-07-14 22:22:16 +0000

    test features

--- vocab_python.py
+++ vocab_python.py
@@ -3,10 +3,14 @@ nombre1 = 5
 nombre2 = 10.45
 nombre3 = 3
 c = -((nombre1+nombre2)-(nombre1*nombre3))+(nombre1+(nombre2*nombre3))
+b = "Chaîne de caractère"
 #branches gitkraken
 
 #initialisation de gitflow
+#merge features
+
 print(c)
 print(nombre1)
-print("Hello aziz !") 
+print("Hello aziz !")
 print(nombre1 + nombre2)
+print(b)
