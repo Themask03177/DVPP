eebac715adb59641e0129905383e14ac658c52ac
Rod <bhazaev@gmail.com>
2020-07-14 23:59:35 +0000

    Ajout des variables quotes et characters

--- first_script/san_antonio.py
+++ first_script/san_antonio.py
@@ -1,1 +1,15 @@
-print("Yo t'as pas un 6")
+#Ecriture du code
+
+Quotes = [
+    "listen to me, Mister Shakespeare : we may or may not be, we are",
+    "we must be able listenning to ourselves or being heard"
+]
+
+Charcters = [
+             "Alvin et les chumpunks",
+             "Betty boot",
+             "Babar",
+             "Calimero",
+             "Caster"
+]
+
