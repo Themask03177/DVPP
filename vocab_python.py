a3fc1bd932e74ae822cd89cb69e6b0a99fe6b5ab
Rod <bhazaev@gmail.com>
2020-07-14 19:45:14 +0000

    Premier test avec pycharm

---
+++ vocab_python.py
@@ -0,0 +1,9 @@
+# ceci est un commentaire
+nombre1 = 5
+nombre2 = 10.45
+nombre3 = 3
+c = -((nombre1+nombre2)-(nombre1*nombre3))+(nombre1+(nombre2*nombre3))
+
+print(c)
+print(nombre1)
+print("Hello world !")
