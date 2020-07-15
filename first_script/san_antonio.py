e708f4bfd1b66f8dafe5545d7d71350a827bc081
Rod <bhazaev@gmail.com>
2020-07-15 12:32:40 +0000

    Amélioration de la fonction Show_Random_Quotes
    application de return et modification du nom de la fonction Show_Random_Quotes par Get_Random_Quotes et ajout du print Get_Random_Quotes

--- first_script/san_antonio.py
+++ first_script/san_antonio.py
@@ -4,19 +4,21 @@ Quotes = ["listen to me, Mister Shakespeare : we may or may not be, we are", "we
 
 Characters = ["Alvin et les chumpunks", "Betty boot", "Babar", "Calimero", "Caster", "Baltazar"]
 
-User_Answer = 'B'
+User_Answer = input("Press Enter for know another quotes or press 'B' for leave program")
 # show random quotes
 
 if User_Answer == "B":
-  pass
+  exit()
 if User_Answer == "C":
   print(" 'C' is'nt a good answer... please enter another letter or press 'B' for leave program")
 else :
   pass
-def Show_Random_Quotes(My_list):
-  Quote = My_list[1]
-  print(Quote)
-
-Show_Random_Quotes(Quotes)
+def Get_Random_Quotes(My_list):
+  # get a random item
+  item = My_list[1]
+  print(item)
+  return "Program is over"
+  # show the quote
+print(Get_Random_Quotes(Quotes))
 
 
