bf3e5d589bbcb4990dc423473b76e6446585af68
Rod <bhazaev@gmail.com>
2020-07-15 11:27:58 +0000

    first test for show random quotes

--- first_script/san_antonio.py
+++ first_script/san_antonio.py
@@ -4,17 +4,19 @@ Quotes = ["listen to me, Mister Shakespeare : we may or may not be, we are", "we
 
 Characters = ["Alvin et les chumpunks", "Betty boot", "Babar", "Calimero", "Caster", "Baltazar"]
 
+User_Answer = 'B'
 # show random quotes
 
-if User_Answer == "B" :
+if User_Answer == "B":
   pass
 if User_Answer == "C":
-  print("'C' is'nt good answers... please enter another answer or enter 'B' For leave the program.")
+  print(" 'C' is'nt a good answer... please enter another letter or press 'B' for leave program")
 else :
-  # - else : show another quote
-def Show_Random_Quote(My_List):
-  Quote = My_List[1]
+  pass
+def Show_Random_Quotes(My_list):
+  Quote = My_list[1]
   print(Quote)
 
-Show_Random_Quote(Quotes)
+Show_Random_Quotes(Quotes)
+
 
