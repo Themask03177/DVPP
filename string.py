24ab62c5dc442ef73f98862d127fba082c98abb1
Rod <bhazaev@gmail.com>
2020-07-20 23:45:32 +0000

    import turtle flocon von kotch and test

--- string.py
+++ string.py
@@ -8,9 +8,39 @@ Hello.lower()
 # number manipulation
 
 (2.5).is_integer()
-
+# import turtle
 import turtle as tu
 tu.fd(50)
 tu.rt(100)
 tu.fd(100)
 tu.speed(1000)
+tu.bye()
+#flocon von Kotch
+
+import turtle as tu
+
+
+def floc(l):
+    if l < 3:
+        tu.fd(l)
+        return
+    floc(l / 3)
+    tu.lt(60)
+    floc(l / 3)
+    tu.rt(120)
+    floc(l / 3)
+    tu.lt(60)
+    floc(l / 3)
+
+
+def flocon(l):
+    tu.speed(0)
+    tu.color('#0000ff', '#55ffff')
+    tu.begin_fill()
+    for i in range(3):
+        floc(l)
+        tu.rt(120)
+    tu.end_fill()
+
+
+flocon(100)
