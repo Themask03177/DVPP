62750a929506b885b9bc3b5e48844347d720029c
Rod <bhazaev@gmail.com>
2020-07-21 00:26:57 +0000

    random quotes

--- first_script/san_antonio.py
+++ first_script/san_antonio.py
@@ -1,3 +1,5 @@
+import random
+
 #Ecriture du code
 
 Quotes = ["listen to me, Mister Shakespeare : we may or may not be, we are", "we must be able listenning to ourselves or being heard"]
@@ -6,9 +8,20 @@ Characters = ["alvin et les chumpunks", "betty boot", "babar", "Calimero", "cast
 
 # définition de la fonction get random
 def Get_Random_Quotes(My_list):
-  # get a random item
-  item = My_list[0]
+  rand_num = random.randint(0, len(My_list) - 1)
+  item = My_list[rand_num]
   return item
+# words capitalize
+def capitalize(Words):
+  for Word in Words :
+    Word.capitalize()
+
+# message character
+
+def message(Character, Quote):
+  capitalize(Character)
+  capitalize(Quote)
+  return "{} a dit : {}".format(Character, Quote)
 
 # Get user answer
 
